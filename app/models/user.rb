54027fedd65b035ad8c839945b0fe94abf98a0ef
gunapriya07 <gunapriya77776@gmail.com>
2025-09-15 10:56:35 +0000

    added the curd operation for the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6}, if: -> { password.present?}
+  enum role: {customer: 0, admin: 1}
 end
